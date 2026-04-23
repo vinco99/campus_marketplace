8b4a21a50303b527bc965f00192b3fee5fa681b4
Nwike Vincent Ikechukwu <111094354+vinco99@users.noreply.github.com>
2026-04-23 13:00:21 +0100

    Wallet model setup

--- campus_xchange/settings.py
+++ campus_xchange/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'users',
     'products',
     'orders',
+    'wallet',
     
+
 ]
 
 MIDDLEWARE = [
---
+++ wallet/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
+class Wallet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+
+class Transaction(models.Model):
+    TRANSACTION_TYPE = [
+        ('credit', 'Credit'),
+        ('debit', 'Debit'),
+    ]
+
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=10, choices=TRANSACTION_TYPE)
+    created_at = models.DateTimeField(auto_now_add=True)
